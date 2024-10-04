d9184d11ad9eb2ad3139fd54e49db5eddd46a467
TBedu <2877181146@qq.com>
2024-10-04 19:53:38 +0800

    Nothing

--- cmd/frpc/sub/root.go
+++ cmd/frpc/sub/root.go
@@ -139,6 +139,7 @@ func startService(
 	log.InitLogger(cfg.Log.To, cfg.Log.Level, int(cfg.Log.MaxDays), cfg.Log.DisablePrintColor)
 
 	if cfgFile != "" {
+		log.Infof("Welcome to TaskBoardFrp!")
 		log.Infof("start frpc service for config file [%s]", cfgFile)
 		defer log.Infof("frpc service for config file [%s] stopped", cfgFile)
 	}
